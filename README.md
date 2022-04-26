ba10b038b955a7eb8e892c4ece7e4438732640bc
Baris Balli <barisbll.dev@gmail.com>
2022-04-26 23:26:31 +0200

    Readme added

---
+++ README.md
@@ -0,0 +1,7 @@
+## Zadanie Kodowania GSD
+
+Witaj GSD 😊,
+
+Dziękuję za danie mi tej możliwości,
+
+Mam nadzieję, że podoba Ci się moje rozwiązanie
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import { get404 } from "./controller/404";
 import createServer from "./util/server";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
 const app = createServer();
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(8080);
 
